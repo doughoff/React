f6c892f02b0b79a2c75f8b8f9fd433d2fb25dc4b
doughoff <email@doughoff.com>
2019-08-15 08:28:44 -0500

    updates

--- my-app/src/experiments/FilterElementsApp.js
+++ my-app/src/experiments/FilterElementsApp.js
@@ -34,52 +34,53 @@ const Filter = (props) => {
   console.log(text);
 
   var keyword = props.lineContains;
-  
+
   var reStr =
-    "\. +"                   + // a preceding sentence-ender, i.e. a period
-                               //   followed by one or more spaces
-    "("                      + // begin remembering the match (i.e. arr[1] below)
-      "[A-Z]"                + // a sentence-starter, i.e. an uppercase letter
-      "("                    + // start of a sentence-continuer, which is either
-        "[^.]"               + // anything but a period
-        "|"                  + // or
-        "\.(?! +[A-Z])"      + // a period not followed by one or more spaces
-                               //   and an uppercase letter
-      ")"                    + // end of a sentence-continuer
-      "*?"                   + // zero or more of the preceding sentence-continuers
-                               //   but as few as possible
-      keyword                + // the keyword being sought
-      "([^.]|\.(?! +[A-Z]))" + // a sentence-continuer, as described above
-      "*?"                   + // zero or more of them but as few as possible
-      "\."                   + // a sentence-ender, i.e. a period
-      "(?= +[A-Z])"          + // followed by one or more spaces and an
-                               //   uppercase letter, which is not remembered
+    "\. +" + // a preceding sentence-ender, i.e. a period
+    //   followed by one or more spaces
+    "(" + // begin remembering the match (i.e. arr[1] below)
+    "[A-Z]" + // a sentence-starter, i.e. an uppercase letter
+    "(" + // start of a sentence-continuer, which is either
+    "[^.]" + // anything but a period
+    "|" + // or
+    "\.(?! +[A-Z])" + // a period not followed by one or more spaces
+    //   and an uppercase letter
+    ")" + // end of a sentence-continuer
+    "*?" + // zero or more of the preceding sentence-continuers
+    //   but as few as possible
+    keyword + // the keyword being sought
+    "([^.]|\.(?! +[A-Z]))" + // a sentence-continuer, as described above
+    "*?" + // zero or more of them but as few as possible
+    "\." + // a sentence-ender, i.e. a period
+    "(?= +[A-Z])" + // followed by one or more spaces and an
+    //   uppercase letter, which is not remembered
     ")";                       // finish remembering the match
-    
-    var re = new RegExp(reStr, "g"); // construct the regular expression
 
-var sentencesWithKeyword = []; // initialize an array to keep the hits
-var arr; // prepare an array to temporarily keep 'exec' return values
-var expandedText = ". " + text + " A";
-// add a sentence-ender (i.e. a period) before the text
-//   and a sentence-starter (i.e. an uppercase letter) after the text
-//   to facilitate finding the first and last sentences
+  let re = new RegExp(reStr, "gi"); // construct the regular expression, global, case insensitive
 
-while ((arr = re.exec(expandedText)) !== null) { // while hits are found
-  sentencesWithKeyword.push(arr[1]); // remember the sentence found
-  re.lastIndex -= 2; // start the next search two characters back
-                     //   to allow for starting the next match
-                     //   with the period that ended the current match
-}
 
-// show the results
-console.log("Text to search:");
-console.log(text);
-console.log("Query string: " + keyword);
-console.log("Hits:");
-for (var num = 0; num < sentencesWithKeyword.length; num += 1) {
-  console.log((num + 1) + ". " + sentencesWithKeyword[num]);
-}
+  let sentencesWithKeyword = []; // initialize an array to keep the hits
+  let arr; // prepare an array to temporarily keep 'exec' return values
+  let expandedText = ". " + text + " A";
+  // add a sentence-ender (i.e. a period) before the text
+  //   and a sentence-starter (i.e. an uppercase letter) after the text
+  //   to facilitate finding the first and last sentences
+
+  // while ((arr = re.exec(expandedText)) !== null) { // while hits are found
+  //   sentencesWithKeyword.push(arr[1]); // remember the sentence found
+  //   re.lastIndex -= 2; // start the next search two characters back
+  //   //   to allow for starting the next match
+  //   //   with the period that ended the current match
+  // }
+
+  // show the results
+  console.log("Text to search:");
+  console.log(text);
+  console.log("Query string: " + keyword);
+  console.log("Hits:");
+  // for (var num = 0; num < sentencesWithKeyword.length; num += 1) {
+  //   console.log((num + 1) + ". " + sentencesWithKeyword[num]);
+  // }
 
   // make an array of the body
   // let linesArray = text.split(/[\.!?]/g);
