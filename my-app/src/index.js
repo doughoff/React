8855b19a899a348dfffc73a8b04d03b22fe5fbbc
doughoff <email@doughoff.com>
2019-12-12 16:06:06 -0600

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './original/index.css';
-import App from './experiments/Dialog';
+import App from './ex13/App';
 import * as serviceWorker from './original/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
